454c6eb16ea88d954d9f67fbf2242fa989fc37db
Bobby Yusuf Hoksono <bobby.hoksono@gmail.com>
2019-05-15 13:26:44 +0700

    Initial Commit

---
+++ LearnerModel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnerModel
+//
+//  Created by Bobby Yusuf Hoksono on 15/5/19.
+//  Copyright © 2019 Bobby Yusuf Hoksono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
